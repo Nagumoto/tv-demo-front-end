38fcf73310ec76dee85d7a43871a515b63d185e1
Neils Madsen <generalyourfather@gmail.com>
2018-12-03 18:34:49 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ManagePage from "./ManagePage";
 import PreviewPage from "./PreviewPage";
-
-const Index = () => <h2>Home</h2>;
 
 const AppRouter = () => (
   <Router>
     <div>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/PreviewPage/">Preview</Link>
-          </li>
-          <li>
-            <Link to="/ManagePage/">Manage</Link>
-          </li>
-        </ul>
+        <h1>
+          <span><Link to="/ManagePage/">Manage</Link></span> <span><Link to="/PreviewPage/">Preview</Link></span>
+        </h1>
       </nav>
-
-      <Route path="/" exact component={Index} />
       <Route path="/PreviewPage/" component={PreviewPage} />
       <Route path="/ManagePage/" component={ManagePage} />
     </div>
