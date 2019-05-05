5d8faf2a28fe7d985a577eab7c1988b73305420f
Neils Madsen <generalyourfather@gmail.com>
2019-05-04 18:34:34 -0600

    Added PUT and DELETE methods

--- src/ManagePage.js
+++ src/ManagePage.js
@@ -27,25 +27,61 @@ class ManagePage extends Component {
         }
     }
 
-    showSelected = () => {
-        console.log('tvShowSelected')
-        console.log(this.props.tvShows.name)
-        console.log(this.props.tvShows.rating)
-        console.log(this.props.tvShows.image)
+    showSelected = (id) => {
+        const show = this.state.tvShows.filter((show) => show._id === id)[0]
         this.setState({
-            name: this.props.tvShows.name,
-            rating: this.props.tvShows.rating,
-            image: this.props.tvShows.image
+            showInProgress: {
+                name: show.name,
+                rating: show.rating,
+                image: show.image
+            }
         })
     }
 
-    showDeleted = () => {
+    showDeleted = async (index) => {
         console.log('tvShowDeleted')
-        this.props.showDeleted()
+        const id = this.state.tvShows[index]._id
+        try {
+            await fetch('https://evening-eyrie-81867.herokuapp.com/shows/' + id, {
+            // await fetch('http://localhost:4000/shows/' + id, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            this.componentDidMount()
+        } catch (err) {
+            console.log(err)
+        }
     }
 
-    postTVShow = async (e) => {
+    saveTVShow = (e) => {
         e.preventDefault()
+        const show = this.state.tvShows.filter((tvShow) => tvShow.name === this.state.showInProgress.name)
+        if (show.length === 0) this.postTVShow()
+        else this.putTVShow(show)
+    }
+
+    putTVShow = async (show) => {
+        console.log(show[0]._id)
+        const id = show[0]._id
+        try {
+            await fetch('https://evening-eyrie-81867.herokuapp.com/shows/' + id, {
+            // await fetch('http://localhost:4000/shows/' + id, {
+                method: 'PUT',
+                body: JSON.stringify(this.state.showInProgress),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(this.componentDidMount())
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    postTVShow = async () => {
         try {
             await fetch('https://evening-eyrie-81867.herokuapp.com/shows/', {
             // await fetch('http://localhost:4000/shows/', {
@@ -88,8 +124,8 @@ class ManagePage extends Component {
     renderShows = () => {
         if (this.state.tvShows) {
             return this.state.tvShows.map(
-                (tvShow) => (
-                    <TVShow name={tvShow.name} key={tvShow._id} allowDelete={true} selectHandler={this.showSelected} deleteHandler={this.showDeleted} />
+                (tvShow, index) => (
+                    <TVShow name={tvShow.name} show={tvShow} index={index} key={tvShow._id} allowDelete={true} selectHandler={this.showSelected} deleteHandler={this.showDeleted} />
                 )
             )
         }
@@ -117,10 +153,16 @@ class ManagePage extends Component {
                                 </div>
                                 <div>
                                     <input onChange={this.changedName} value={this.state.showInProgress.name} /><br />
-                                    <input onChange={this.changedRating} value={this.state.showInProgress.rating} /><br />
+                                    <select value={this.state.showInProgress.rating} onChange={this.changedRating}>
+                                        <option>{1}</option>
+                                        <option>{2}</option>
+                                        <option>{3}</option>
+                                        <option>{4}</option>
+                                        <option>{5}</option>
+                                    </select><br />
                                     <input onChange={this.changedimage} value={this.state.showInProgress.image} /><br />
                                     <span id="create-update">
-                                        <button onClick={this.postTVShow}>Create/Update</button>
+                                        <button onClick={this.saveTVShow}>Create/Update</button>
                                     </span>
                                 </div>
                             </div>
--- src/TVShow.js
+++ src/TVShow.js
@@ -5,25 +5,34 @@ class TVShow extends Component {
     
     static propTypes = {
         name: PropTypes.string.isRequired,
+        index: PropTypes.number.isRequired,
         allowDelete: PropTypes.bool,
         selectHandler: PropTypes.func.isRequired,
         deleteHandler: PropTypes.func
     }
 
+    deleteHandler = (e) => {
+        e.preventDefault()
+        this.props.deleteHandler(this.props.index)
+    }
+
+    selectHandler = () => {
+        this.props.selectHandler(this.props.show._id)
+    }
+
     renderDelete = () => {
         if (this.props.allowDelete) {
             return (
-                <button onClick={this.props.deleteHandler} className="delete">-</button>
+                <button onClick={this.deleteHandler} className="delete">-</button>
             )
         }
-        
     }
 
     render = () => {
         return (
                 <div>
                     <span>
-                    <button onClick={this.props.selectHandler}>{this.props.name}</button>
+                    <button onClick={this.selectHandler}>{this.props.name}</button>
                     </span>
                     {this.renderDelete()}
                 </div>
