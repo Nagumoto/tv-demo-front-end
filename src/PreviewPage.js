97b21470a60d8b005d7c4c9c3b7bd6f55842f7e8
Neils Madsen <generalyourfather@gmail.com>
2019-05-04 18:35:23 -0600

    improved PreviewPage useability

--- src/PreviewPage.js
+++ src/PreviewPage.js
@@ -1,49 +1,76 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import SiteNav from './SiteNav'
 import TVShow from './TVShow'
 
 class PreviewPage extends Component {
-
-    static propTypes = {
-        tvShows: PropTypes.array.isRequired
-    }
 
     state = {
         selectedShow: {
             name: '',
             rating: '',
             image: ''
+        },
+        filter: true,
+        tvShows: []
+    }
+
+    componentDidMount = async () => {
+        try {
+            const res = await fetch('https://evening-eyrie-81867.herokuapp.com/shows/')
+            // const res = await fetch('http://localhost:4000/shows/')
+            const tvShows = await res.json()
+            this.setState({ tvShows }, () => {
+                console.log('Shows fetched...', this.state.tvShows)
+            })
+            this.setState({
+                selectedShow: this.state.tvShows[0]
+            })
+        } catch (err) {
+            console.log(err)
         }
     }
 
-    showSelected = () => {
+    filterButton = () => {
+        const filter = this.state.filter
+        return filter ? 'On' : 'Off'
+    }
+
+    filterChange = () => {
+        this.setState({ filter: !this.state.filter })
+    }
+
+    showSelected = (id) => {
+        const show = this.state.tvShows.filter((show) => show._id === id)[0]
         this.setState({
             selectedShow: {
-                name: this.props.tvShows.name,
-                rating: this.props.show.rating,
-                image: this.props.show.image
+                name: show.name,
+                rating: show.rating,
+                image: show.image
             }
         })
     }
 
     renderShows = () => {
-        if (this.props.tvShows) {
-            return this.props.tvShows
+        if (this.state.filter) {
+            return this.state.tvShows
                 .filter((tvShow) => {
                     return tvShow.rating < 4
                 })
-                .map(
-                    (tvShow) => (
-                        <TVShow name={tvShow.name} key={tvShow.name} selectHandler={this.showSelected} />
-                    )
+                .map((tvShow, index) => (
+                    <TVShow name={tvShow.name} show={tvShow} index={index} key={tvShow._id} selectHandler={this.showSelected} />
                 )
+                )
+        } else {
+            return this.state.tvShows.map((tvShow, index) => (
+                <TVShow name={tvShow.name} show={tvShow} index={index} key={tvShow._id} selectHandler={this.showSelected} />
+            )
+            )
         }
     }
 
     calculateAvgRating = () => {
-        return this.props.tvShows.reduce((sum, tvShow) => {
-            return sum + tvShow.rating / this.props.tvShows.length
+        return this.state.tvShows.reduce((sum, tvShow) => {
+            return sum + tvShow.rating / this.state.tvShows.length
         }, 0)
     }
 
@@ -64,11 +91,12 @@ class PreviewPage extends Component {
                 <div>
                     <section id="show-selection">
                         <h2>Shows</h2>
+                        <section>Safety Filter: <button onClick={this.filterChange} >{this.filterButton()}</button></section>
                         <section>Average Rating: {this.calculateAvgRating()}</section>
                         {this.renderShows()}
                     </section>
                     <div id="show-preview">
-                        <h3><span>{this.state.selectedShow.name}</span><span>{this.state.selectedShow.rating}</span></h3>
+                        <h3><span>Show: {this.state.selectedShow.name}</span><span>Rating: {this.state.selectedShow.rating}</span></h3>
                         <div className="center">
                             {this.renderImage()}
                         </div>
